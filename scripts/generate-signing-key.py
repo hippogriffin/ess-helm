510c831d3c9011d6f89c23592c5675f0889a6787
Ben Banfield-Zanin <benbz@matrix.org>
2024-11-22 14:22:31 +0000

    Run through reuse annotate so everything has the same format

--- scripts/generate-signing-key.py
+++ scripts/generate-signing-key.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
+
 # Copyright 2024 New Vector Ltd
 #
 # SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-Element-Commercial
+
 import signedjson.key
 
 signing_key = signedjson.key.generate_signing_key(0)
