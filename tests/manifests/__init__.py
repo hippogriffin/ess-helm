1d5d2a6ed55ae4d226deaa7447d7bb03b6911cef
Ben Banfield-Zanin <benbz@matrix.org>
2024-12-11 11:12:09 +0000

    Set hyphenated_name in the same way as has_ingress

--- tests/manifests/__init__.py
+++ tests/manifests/__init__.py
@@ -13,9 +13,8 @@ _raw_component_details = {
 
 def _enrich_components_to_test() -> Dict[str, Any]:
     _component_details = copy.deepcopy(_raw_component_details)
-    for component, details in _raw_component_details.items():
-        if "hyphened_name" not in details:
-            _component_details[component]["hyphened_name"] = component
+    for component in _raw_component_details:
+        _component_details[component].setdefault("hyphened_name", component)
 
         _component_details[component]["minimal_values_file"] = (
             f"{_component_details[component]["hyphened_name"]}-minimal-values.yaml"
