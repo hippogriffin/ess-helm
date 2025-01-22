f37d3dc4fcef423adbb8971295caff8c03c6ba59
Ben Banfield-Zanin <benbz@matrix.org>
2025-01-22 15:59:42 +0000

    Scripts: have reuse ignore the Copyright line from the reuse annotate command

--- scripts/assemble_helm_charts_from_fragments.sh
+++ scripts/assemble_helm_charts_from_fragments.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Copyright 2024 New Vector Ltd
+# Copyright 2024-2025 New Vector Ltd
 #
 # SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-Element-Commercial
 
@@ -22,7 +22,9 @@ function assemble_helm_chart_from_fragments() {
   echo "Building $chart_dir"
   "$scripts_dir/construct_helm_schema.py" "$chart_dir/source/values.schema.json" "$chart_dir/values.schema.json"
   "$scripts_dir/construct_helm_values.py" "$chart_dir/source/values.yaml.j2" "$chart_dir/values.yaml"
+  # REUSE-IgnoreStart
   reuse annotate --copyright="Copyright 2024 New Vector Ltd" --license "AGPL-3.0-only OR LicenseRef-Element-Commercial" "$chart_dir/values.yaml"
+  # REUSE-IgnoreEnd
 }
 
 [ ! -d "$chart_root" ] && echo "$chart_root must be a directory that exists" && exit 1
