8dd8cbbe894365d361412b57eb23be91d3aa2db6
Ben Banfield-Zanin <benbz@matrix.org>
2025-02-06 16:47:45 +0000

    Manifest Tests: improve error message for test_values_file_renders_only_itself

--- tests/manifests/test_basic.py
+++ tests/manifests/test_basic.py
@@ -25,4 +25,6 @@ async def test_values_file_renders_only_itself(release_name, component, template
     for shared_component in component_details[component].get("shared_components", []):
         allowed_starts_with.append(f"{release_name}-{shared_component}")
     for template in templates:
-        assert any(template["metadata"]["name"].startswith(allowed_start) for allowed_start in allowed_starts_with)
+        assert any(template["metadata"]["name"].startswith(allowed_start) for allowed_start in allowed_starts_with), (
+            f"{[template['metadata']['name']]} does not start with one of {allowed_starts_with}"
+        )
