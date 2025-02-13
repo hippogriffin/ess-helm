e76043a301a9c85ca173341d0c45f2f7e7f92425
Gaël Goinvic <gaelg@element.io>
2025-02-13 16:44:47 +0100

    postgres hash: fix wrong essPassword in MAS

--- charts/matrix-stack/templates/ess-library/_credentials.tpl
+++ charts/matrix-stack/templates/ess-library/_credentials.tpl
@@ -128,30 +128,3 @@ SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-Element-Commercial
 {{- end -}}
 {{- end -}}
 
-
-{{- define "element-io.ess-library.postgres-label" -}}
-{{- $root := .root -}}
-{{- with required "element-io.ess-library.postgres-label requires context" .context -}}
-{{- $essPassword := required "element-io.ess-library.postgres-label context missing essPassword" .essPassword -}}
-{{- $postgresProperty := required "elment-io.ess-library.postgres-label context missing postgresProperty" .postgresProperty -}}
-k8s.element.io/postgresPasswordHash: {{ if $postgresProperty -}}
-    {{- if $postgresProperty.password.value -}}
-    {{- $postgresProperty.password.value | sha1sum -}}
-    {{- else -}}
-    {{- (printf "%s-%s" (tpl $postgresProperty.password.secret $root) $postgresProperty.password.secretKey) | sha1sum -}}
-    {{- end -}}
-{{- else if (index $root.Values.postgres.essPasswords $essPassword) }}
-    {{- if (index $root.Values.postgres.essPasswords $essPassword).value -}}
-    {{- (index $root.Values.postgres.essPasswords $essPassword).value | sha1sum -}}
-    {{- else -}}
-    {{- (printf "%s-%s" (tpl (index $root.Values.postgres.essPasswords $essPassword).secret $root) (index $root.Values.postgres.essPasswords $essPassword).secretKey) | sha1sum -}}
-    {{- end -}}
-{{- else -}}
-  {{- if $root.Values.initSecrets.enabled -}}
-  {{- (printf "%s-generated" $root.Release.Name) | sha1sum  -}}
-  {{- else -}}
-  {{- fail "No postgres password set and initSecrets not set" -}}
-  {{- end -}}
-{{- end -}}
-{{- end -}}
-{{- end -}}
--- tests/manifests/test_labels.py
+++ tests/manifests/test_labels.py
@@ -2,9 +2,11 @@
 #
 # SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-Element-Commercial
 
+from hashlib import sha1
+
 import pytest
 
-from . import values_files_to_test
+from . import component_details, secrets_values_files_to_test, values_files_to_test
 
 
 @pytest.mark.parametrize("values_file", values_files_to_test)
@@ -48,3 +50,34 @@ async def test_templates_have_expected_labels(release_name, templates):
             "is not a concatenation of the expected chart release name of '{release_name}' and the instance label. "
             f"The label value is {labels['app.kubernetes.io/instance']} vs {labels['app.kubernetes.io/name']}"
         )
+
+
+@pytest.mark.parametrize("values_file", secrets_values_files_to_test)
+@pytest.mark.asyncio_cooperative
+async def test_templates_have_postgres_hash_label(release_name, component, templates, values):
+    for template in templates:
+        if template["kind"] in ["Deployment", "StatefulSet", "Job"]:
+            id = f"{template['kind']}/{template['metadata']['name']}"
+            if not template["metadata"]["name"].endswith(component_details[component].get("hyphened_name", component)):
+                continue
+            labels = template["spec"]["template"]["metadata"]["labels"]
+            if "postgres" in component_details[component].get("shared_components", []):
+                assert "k8s.element.io/postgresPasswordHash" in labels, (
+                    f"{id} does not have postgres password hash label"
+                )
+                if values[component].get("postgres", {}).get("password", {}).get("value", None):
+                    expected = values[component]["postgres"]["password"]["value"]
+                elif values[component].get("postgres", {}).get("password", {}).get("secret", None):
+                    secret_name = values[component]["postgres"]["password"]["secret"]
+                    expected = f"{secret_name}-{values[component]['postgres']['password']['secretKey']}"
+                elif values["postgres"].get("essPasswords", {}).get(component, {}).get("value", None):
+                    expected = values["postgres"]["essPasswords"][component]["value"]
+                elif values["postgres"].get("essPasswords", {}).get(component, {}).get("secret", None):
+                    secret_name = values["postgres"]["essPasswords"][component]["secret"]
+                    expected = f"{secret_name}-{values['postgres']['essPasswords'][component]['secretKey']}"
+                else:
+                    expected = f"{release_name}-generated"
+                expected = expected.replace("{{ $.Release.Name }}", release_name)
+                assert labels["k8s.element.io/postgresPasswordHash"] == sha1(expected.encode()).hexdigest(), (
+                    f"{id} has incorrect postgres password hash, expect {expected} hashed as sha1"
+                )
