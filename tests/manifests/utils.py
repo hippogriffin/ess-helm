8ed7f9654bc0f2a207e0ebdc177ac06b8a7c3427
Gaël Goinvic <gaelg@element.io>
2025-02-27 12:40:13 +0100

    manifest tests: skip some workloads properties unless we force it

--- tests/manifests/utils.py
+++ tests/manifests/utils.py
@@ -167,22 +167,23 @@ def make_templates(chart: pyhelm3.Chart, release_name: str):
     return _make_templates
 
 
-def iterate_component_parts(component, values, setter, if_condition):
+def iterate_component_parts(component, values, setter, if_condition, force):
     if component_details[component][if_condition]:
         setter(values[component], values)
         for sub_component in component_details[component]["sub_components"]:
-            setter(values[component].setdefault(sub_component, {}), values)
+            if force or not component_details[component]["sub_components"][sub_component]["uses_parent_properties"]:
+                setter(values[component].setdefault(sub_component, {}), values)
     for shared_component in component_details[component].get("shared_components", []):
         if shared_components_details[shared_component][if_condition]:
             setter(values.setdefault(shared_component, {}), values)
 
 
-def iterate_component_workload_parts(component, values, setter):
-    iterate_component_parts(component, values, setter, "has_workloads")
+def iterate_component_workload_parts(component, values, setter, force=False):
+    iterate_component_parts(component, values, setter, "has_workloads", force)
 
 
 def iterate_component_image_parts(component, values, setter):
-    iterate_component_parts(component, values, setter, "has_image")
+    iterate_component_parts(component, values, setter, "has_image", False)
 
 
 def get_or_empty(d, key):
