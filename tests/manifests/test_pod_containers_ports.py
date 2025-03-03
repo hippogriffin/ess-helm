651b97f234fdc8c0d0cc7efe7b08aa5445850599
Ben Banfield-Zanin <benbz@matrix.org>
2025-03-03 09:32:42 +0000

    Ensure all ports have names

--- tests/manifests/test_pod_containers_ports.py
+++ tests/manifests/test_pod_containers_ports.py
@@ -17,3 +17,20 @@ async def test_unique_ports_in_containers(templates):
             for container in template["spec"]["template"]["spec"]["containers"]:
                 ports += [port["containerPort"] for port in container.get("ports", [])]
             assert len(ports) == len(set(ports)), f"Ports are not unique: {id}, {ports}"
+
+
+@pytest.mark.parametrize("values_file", values_files_to_test)
+@pytest.mark.asyncio_cooperative
+async def test_ports_in_containers_are_named(templates):
+    for template in templates:
+        if template["kind"] in ["Deployment", "StatefulSet", "Job"]:
+            id = f"{template['kind']}/{template['metadata']['name']}"
+
+            port_names = []
+            for container in template["spec"]["template"]["spec"]["containers"]:
+                for port in container.get("ports", []):
+                    assert "name" in port, (
+                        f"{id} has container {container['name']} which has a port without a name: {port}"
+                    )
+                    port_names.append(port["name"])
+            assert len(port_names) == len(set(port_names)), f"Port names are not unique: {id}, {port_names}"
---
+++ tests/manifests/test_services.py
@@ -0,0 +1,23 @@
+# Copyright 2025 New Vector Ltd
+#
+# SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-Element-Commercial
+
+import pytest
+
+from . import values_files_to_test
+
+
+@pytest.mark.parametrize("values_file", values_files_to_test)
+@pytest.mark.asyncio_cooperative
+async def test_ports_in_services_are_named(templates):
+    for template in templates:
+        if template["kind"] == "Service":
+            id = f"{template['kind']}/{template['metadata']['name']}"
+            assert "ports" in template["spec"], f"{id} does not specify a ports list"
+            assert len(template["spec"]["ports"]) > 0, f"{id} does not include any ports"
+
+            port_names = []
+            for port in template["spec"]["ports"]:
+                assert "name" in port, f"{id} has a port without a name: {port}"
+                port_names.append(port["name"])
+            assert len(port_names) == len(set(port_names)), f"Port names are not unique: {id}, {port_names}"
